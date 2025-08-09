d1ae9be2d5ec5ca864367c72a90abf0842fec34c
Alexander Hörst <hoerst.alexander@gmail.com>
2025-08-09 12:51:08 +0200

    create user login and registrations cases for videoflix

--- core/settings.py
+++ core/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
     'django_filters',
     'django_rq',
     'video_app',
@@ -198,4 +199,5 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'BLACKLIST_AFTER_ROTATION': True,
 }
